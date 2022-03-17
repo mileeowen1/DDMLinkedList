a43db7a9d1c1e1c4c7a24786d68f898e4c2deaee
Milee Owen <101599501+mileeowen1@users.noreply.github.com>
2022-03-17 10:17:16 -0700

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+/*
+Name: Milee Owen
+Class: CPSC 122, Spring 2020
+Date: March 29, 2020
+Programming Assignment: PA6
+Description: Create a program that would arrange our music library based attributes such as song title, song artist, song genre, and rating. This assignment is a digital music manager (DMM) for managing a music library using linked list.
+Notes: Main cpp file
+*/
+#include "PA6.h"
+
+int main() {
+
+	// runs program
+	runDigitalMusicManager();
+
+	return 0;
+}
